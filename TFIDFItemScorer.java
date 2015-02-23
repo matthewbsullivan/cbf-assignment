c1a042934a2d49551b537d638b51d3001a1e972e
matthewbsullivan <sully101@gmail.com>
2015-02-22 18:20:32 -0600

    Initial commit

---
+++ TFIDFItemScorer.java
@@ -0,0 +1,72 @@
+package edu.umn.cs.recsys.cbf;
+
+import org.grouplens.lenskit.basic.AbstractItemScorer;
+import org.grouplens.lenskit.data.dao.UserEventDAO;
+import org.grouplens.lenskit.data.event.Rating;
+import org.grouplens.lenskit.data.history.UserHistory;
+import org.grouplens.lenskit.data.pref.Preference;
+import org.grouplens.lenskit.vectors.MutableSparseVector;
+import org.grouplens.lenskit.vectors.SparseVector;
+import org.grouplens.lenskit.vectors.VectorEntry;
+
+import javax.annotation.Nonnull;
+import javax.inject.Inject;
+import java.util.List;
+
+/**
+ * @author <a href="http://www.grouplens.org">GroupLens Research</a>
+ */
+public class TFIDFItemScorer extends AbstractItemScorer {
+    private final UserEventDAO dao;
+    private final TFIDFModel model;
+    private final UserProfileBuilder profileBuilder;
+
+    /**
+     * Construct a new item scorer.  LensKit's dependency injector will call this constructor and
+     * provide the appropriate parameters.
+     *
+     * @param dao The user-event DAO, so we can fetch a user's ratings when scoring items for them.
+     * @param m   The precomputed model containing the item tag vectors.
+     * @param upb The user profile builder for building user tag profiles.
+     */
+    @Inject
+    public TFIDFItemScorer(UserEventDAO dao, TFIDFModel m, UserProfileBuilder upb) {
+        this.dao = dao;
+        model = m;
+        profileBuilder = upb;
+    }
+
+    /**
+     * Generate item scores personalized for a particular user.  For the TFIDF scorer, this will
+     * prepare a user profile and compare it to item tag vectors to produce the score.
+     *
+     * @param user   The user to score for.
+     * @param output The output vector.  The contract of this method is that the caller creates a
+     *               vector whose possible keys are all items that should be scored; this method
+     *               fills in the scores.
+     */
+    @Override
+    public void score(long user, @Nonnull MutableSparseVector output) {
+        // Get the user's ratings
+        UserHistory<Rating> ratings = dao.getEventsForUser(user, Rating.class);
+        if (ratings == null) {
+            // the user doesn't exist
+            return;
+        }
+
+        // Get the user's profile, which is a vector with their 'like' for each tag
+        SparseVector userVector = profileBuilder.makeUserProfile(ratings);
+
+        // Loop over each item requested and score it.
+        // The *domain* of the output vector is the items that we are to score.
+        for (VectorEntry e: output.view(VectorEntry.State.EITHER)) {
+            // Score the item represented by 'e'.
+            // Get the item vector for this item
+            SparseVector iv = model.getItemVector(e.getKey());
+            // TODO Compute the cosine of this item and the user's profile, store it in the output vector
+            output.set(e.getKey(), userVector.dot(iv) / (userVector.norm() * iv.norm()));
+            // TODO And remove this exception to say you've implemented it
+            //throw new UnsupportedOperationException("stub implementation"); ...Implemented.
+        }
+    }
+}
---
+++ WeightedUserProfileBuilder.java
@@ -0,0 +1,46 @@
+package edu.umn.cs.recsys.cbf;
+
+import org.grouplens.lenskit.data.event.Rating;
+import org.grouplens.lenskit.data.history.RatingVectorUserHistorySummarizer;
+import org.grouplens.lenskit.data.history.UserHistory;
+import org.grouplens.lenskit.vectors.MutableSparseVector;
+import org.grouplens.lenskit.vectors.SparseVector;
+import org.grouplens.lenskit.vectors.VectorEntry;
+
+import javax.annotation.Nonnull;
+import javax.inject.Inject;
+
+/**
+ * Build a user profile from all positive ratings.
+ */
+public class WeightedUserProfileBuilder implements UserProfileBuilder {
+    /**
+     * The tag model, to get item tag vectors.
+     */
+    private final TFIDFModel model;
+
+    @Inject
+    public WeightedUserProfileBuilder(TFIDFModel m) {
+        model = m;
+    }
+
+    @Override
+    public SparseVector makeUserProfile(@Nonnull UserHistory<Rating> history) {
+        // Create a new vector over tags to accumulate the user profile
+        MutableSparseVector profile = model.newTagVector();
+        // Fill it with 0's initially - they don't like anything
+        profile.fill(0);
+
+        // Convert the user's ratings to a vector (makes normalization easier).
+        MutableSparseVector ratingVector =
+                RatingVectorUserHistorySummarizer.makeRatingVector(history)
+                                                 .mutableCopy();
+
+        // TODO Normalize the user's ratings
+        // TODO Build the user's weighted profile
+
+        // The profile is accumulated, return it.
+        // It is good practice to return a frozen vector.
+        return profile.freeze();
+    }
+}
